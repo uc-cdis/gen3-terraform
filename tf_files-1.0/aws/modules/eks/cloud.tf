dca1b2d29d265cc451be2809267d23c2057d31ef
Edward Malinowski <edwardmalinowski@Eds-MacBook-Pro.attlocal.net>
2023-08-07 12:10:48 -0500

    feat(terraform): Updated commons module to be more generic and user friendly

--- tf_files-1.0/aws/modules/eks/cloud.tf
+++ tf_files-1.0/aws/modules/eks/cloud.tf
@@ -590,7 +590,7 @@ resource "aws_iam_service_linked_role" "autoscaling" {
   aws_service_name = "autoscaling.amazonaws.com"
   custom_suffix = var.vpc_name
   lifecycle {
-    ignore_changes  = ["custom_suffix"]
+    ignore_changes  = [custom_suffix]
   }
 }
 
