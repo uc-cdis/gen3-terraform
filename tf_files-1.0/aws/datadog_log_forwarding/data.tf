182b9728d0b671316da9f26a2d494a633ca8025a
Aidan Hilt <ahilt@uchicago.edu>
2023-10-18 18:04:29 -0400

    Need to update references everywhere

--- tf_files-1.0/aws/datadog_log_forwarding/data.tf
+++ tf_files-1.0/aws/datadog_log_forwarding/data.tf
@@ -1,13 +1,13 @@
-data "aws_iam_policy_document" "backup_bucket_writer" {
+data "aws_iam_policy_document" "fargate_logs_backup_bucket_writer" {
   statement {
     actions   = ["s3:Get*","s3:List*"]
     effect    = "Allow"
-    resources = [aws_s3_bucket.backup_bucket.arn, "${aws_s3_bucket.backup_bucket.arn}/*"]
+    resources = [aws_s3_bucket.fargate_logs_backup_bucket.arn, "${aws_s3_bucket.fargate_logs_backup_bucket.arn}/*"]
   }
 
   statement {
     effect    = "Allow"
     actions   = ["s3:PutObject","s3:GetObject","s3:DeleteObject"]
-    resources = ["${aws_s3_bucket.backup_bucket.arn}/*"]
+    resources = ["${aws_s3_bucket.fargate_logs_backup_bucket.arn}/*"]
   }
 }
