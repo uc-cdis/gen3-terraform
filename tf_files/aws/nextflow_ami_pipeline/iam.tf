a9f79f3d86f9c99d82611d42150ec50f59d65865
Jawad Qureshi <qureshi@uchicago.edu>
2024-01-03 11:10:22 -0600

    Add support for multiple pipelines reusing IAM

--- tf_files/aws/nextflow_ami_pipeline/iam.tf
+++ tf_files/aws/nextflow_ami_pipeline/iam.tf
@@ -1,9 +1,22 @@
 ## IAM Instance Profile for image builder
 
+locals {
+  image_builder_iam_role = data.aws_iam_role.existing_image_builder == null ? aws_iam_role.image_builder.name : data.aws_iam_role.existing_image_builder.name
+}
+
+# Attempt to fetch the existing IAM role
+data "aws_iam_role" "existing_image_builder" {
+  name = "EC2InstanceProfileForImageBuilder-nextflow"
+  # This will fail if the role does not exist
+}
+
+# Conditionally create the IAM role if it does not already exist
 resource "aws_iam_role" "image_builder" {
+  count              = data.aws_iam_role.existing_image_builder.id == null ? 1 : 0
   name               = "EC2InstanceProfileForImageBuilder-nextflow"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json 
 }
+
 
 data "aws_iam_policy_document" "assume_role" {
   statement {
@@ -16,21 +29,21 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "amazon_ssm" {
-  role       = aws_iam_role.image_builder.name
+  role       = local.image_builder_iam_role
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role_policy_attachment" "image_builder" {
-  role       = aws_iam_role.image_builder.name
+  role       = local.image_builder_iam_role
   policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilder"
 }
 
 resource "aws_iam_role_policy_attachment" "image_builder_ecr" {
-  role       = aws_iam_role.image_builder.name
+  role       = local.image_builder_iam_role
   policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
 }
 
 resource "aws_iam_instance_profile" "image_builder" {
   name = "image-builder-profile"
-  role = aws_iam_role.image_builder.name
+  role = local.image_builder_iam_role
 }
