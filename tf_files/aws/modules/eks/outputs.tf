cc7c1113d705681082703b4efe3cbb090ee8ae84
Aidan Hilt <ahilt@uchicago.edu>
2024-03-04 15:14:38 -0500

    I think I'm dumb

--- tf_files/aws/modules/eks/outputs.tf
+++ tf_files/aws/modules/eks/outputs.tf
@@ -27,5 +27,5 @@ output "cluster_oidc_provider_url" {
 }
 
 output "cluster_oidc_provider_arn" {
-  value = aws_eks_cluster.aws_iam_openid_connect_provider.identity_provider.arn
+  value = aws_iam_openid_connect_provider.identity_provider[0].arn
 }
