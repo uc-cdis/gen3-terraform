dcf7bb402249f79580c07101f7c8bb5660051ffd
EliseCastle23 <elisemcastle@uchicago.edu>
2024-05-10 10:22:33 -0600

    re-enabling special characters, but removing the percent sign

--- tf_files/aws/aurora_db/root.tf
+++ tf_files/aws/aurora_db/root.tf
@@ -50,8 +50,8 @@ resource "aws_iam_role_policy_attachment" "new_attach" {
 resource "random_password" "db_password" {
   count            = var.password != "" ? 0 : 1
   length           = 16
-  special          = false
-  override_special = "_%@"
+  special          = true
+  override_special = "_@"
 }
 
 resource "null_resource" "db_setup" {
