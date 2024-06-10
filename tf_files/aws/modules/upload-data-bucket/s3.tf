e92189d9faa0cc609e8ad0b6e5406e831fc1bea3
Edward Malinowski <edwardmalinowski@Eds-MacBook-Pro.attlocal.net>
2024-06-10 10:14:24 -0500

    feat(gen3-module): Updated gen3 module to improve deployment experience

--- tf_files/aws/modules/upload-data-bucket/s3.tf
+++ tf_files/aws/modules/upload-data-bucket/s3.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "data_bucket" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm = "aws:kms"
     }
   }
 }
@@ -61,7 +61,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm = "aws:kms"
     }
   }
 }
