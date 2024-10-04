c3d618d0b46921dd1687c0e73410106bdc7ccc43
EliseCastle23 <109446148+EliseCastle23@users.noreply.github.com>
2024-10-04 09:15:15 -0600

    fixing outputs

--- tf_files/aws/commons/outputs.tf
+++ tf_files/aws/commons/outputs.tf
@@ -121,5 +121,5 @@ output "es_endpoint" {
 
 output "waf_arn" {
   description = "WAF arn - annotate the cluster ingress"
-  value       = aws_wafv2_web_acl.waf.arn
+  value       = module.aws_waf[0].aws_wafv2_web_acl.waf.arn
 }
--- tf_files/aws/waf/output.tf
+++ tf_files/aws/waf/output.tf
@@ -4,5 +4,5 @@
 
 output "waf_arn" {
   description = "WAF arn - annotate the cluster ingress"
-  value       = aws_wafv2_web_acl.waf.arn
+  value       = module.aws_waf[0].aws_wafv2_web_acl.waf.arn
 }
