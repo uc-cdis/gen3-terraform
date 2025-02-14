8593ce3a0b6af5846e5b3acbf055f76a44d1903b
Aidan Hilt <ahilt@uchicago.edu>
2025-02-14 14:03:06 -0500

    Re-enabling just the nodepool

--- tf_files/aws/modules/eks/karpenter.tf
+++ tf_files/aws/modules/eks/karpenter.tf
@@ -325,66 +325,66 @@ resource "helm_release" "karpenter" {
   depends_on = [time_sleep.wait_60_seconds]
 }
 
-# resource "kubectl_manifest" "karpenter_node_pool" {
-#   count   = var.use_karpenter ? 1 : 0
+resource "kubectl_manifest" "karpenter_node_pool" {
+  count   = var.use_karpenter ? 1 : 0
 
-#   yaml_body = <<-YAML
-#     ---
-#     apiVersion: karpenter.sh/v1beta1
-#     kind: NodePool
-#     metadata:
-#       name: default
-#     spec:
-#       disruption:
-#         consolidateAfter: 30s
-#         consolidationPolicy: WhenEmpty
-#         expireAfter: "168h"
-#       limits:
-#         cpu: "1000"
-#         memory: 1000Gi
-#       template:
-#         metadata:
-#           labels:
-#             role: default
-#         spec:
-#           kubelet:
-#             evictionHard:
-#               memory.available: 5%
-#             evictionSoft:
-#               memory.available: 10%
-#             evictionSoftGracePeriod:
-#               memory.available: 5m
-#             kubeReserved:
-#               cpu: 480m
-#               ephemeral-storage: 3Gi
-#               memory: 1632Mi
-#           nodeClassRef:
-#             apiVersion: karpenter.k8s.aws/v1beta1
-#             kind: EC2NodeClass
-#             name: default
-#           requirements:
-#           - key: karpenter.sh/capacity-type
-#             operator: In
-#             values:
-#             - on-demand
-#             - spot
-#           - key: kubernetes.io/arch
-#             operator: In
-#             values:
-#             - amd64
-#           - key: karpenter.k8s.aws/instance-category
-#             operator: In
-#             values:
-#             - c
-#             - m
-#             - r
-#             - t
-#   YAML
+  yaml_body = <<-YAML
+    ---
+    apiVersion: karpenter.sh/v1beta1
+    kind: NodePool
+    metadata:
+      name: default
+    spec:
+      disruption:
+        consolidateAfter: 30s
+        consolidationPolicy: WhenEmpty
+        expireAfter: "168h"
+      limits:
+        cpu: "1000"
+        memory: 1000Gi
+      template:
+        metadata:
+          labels:
+            role: default
+        spec:
+          kubelet:
+            evictionHard:
+              memory.available: 5%
+            evictionSoft:
+              memory.available: 10%
+            evictionSoftGracePeriod:
+              memory.available: 5m
+            kubeReserved:
+              cpu: 480m
+              ephemeral-storage: 3Gi
+              memory: 1632Mi
+          nodeClassRef:
+            apiVersion: karpenter.k8s.aws/v1beta1
+            kind: EC2NodeClass
+            name: default
+          requirements:
+          - key: karpenter.sh/capacity-type
+            operator: In
+            values:
+            - on-demand
+            - spot
+          - key: kubernetes.io/arch
+            operator: In
+            values:
+            - amd64
+          - key: karpenter.k8s.aws/instance-category
+            operator: In
+            values:
+            - c
+            - m
+            - r
+            - t
+  YAML
 
-#   depends_on = [
-#     helm_release.karpenter
-#   ]
-# }
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
 
 # resource "kubectl_manifest" "karpenter_node_class" {
 #   count   = var.use_karpenter ? 1 : 0
