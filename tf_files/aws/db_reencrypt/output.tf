0f63938b7d5e08c7e28bb5744e4b5a827898446e
Ed <emalinowski@uchicago.edu>
2025-03-11 15:43:09 -0500

    Removed import for new bucket

--- tf_files/aws/db_reencrypt/output.tf
+++ tf_files/aws/db_reencrypt/output.tf
@@ -1,9 +1,9 @@
 
 output "identifier" {
-  value = aws_rds_cluster_instance.postgresql.*.identifier
+  value = aws_rds_cluster_instance.postgresql.0.identifier
 }
 
 
 output "cluster_identifier" {
-  value = aws_rds_cluster.postgresql.*.cluster_identifier
+  value = aws_rds_cluster.postgresql.0.cluster_identifier
 }
