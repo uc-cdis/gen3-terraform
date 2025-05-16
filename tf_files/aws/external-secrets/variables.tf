691dd471c62f91ec25bf5851ea389b953d996743
Aidan Hilt <ahilt@uchicago.edu>
2025-05-16 14:51:20 -0400

    It's an ID, not an ARN

--- tf_files/aws/external-secrets/variables.tf
+++ tf_files/aws/external-secrets/variables.tf
@@ -2,4 +2,4 @@ variable commons_name {
   description = "This is the name of the specific commons you are running. Each commons will only have access to its own secrets"
 }
 
-variable oidc_provider_arn {}
+variable oidc_provider_id {}
