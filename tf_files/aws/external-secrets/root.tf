3e440eef8b3a4c00b69a59ad4651fd297812f180
Aidan Hilt <ahilt@uchicago.edu>
2025-05-16 14:14:45 -0400

    Creating a standalone module for external secrets

---
+++ tf_files/aws/external-secrets/root.tf
@@ -0,0 +1,60 @@
+resource "aws_iam_role" "external-secrets-role" {
+  name = "${var.commons_name}-external-secrets-sa"
+  description = "Role for external-secrets service account for ${var.commons_name}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
+      {
+        Sid = ""
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${var.oidc_provider_arn}"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${var.oidc_provider_arn}:sub" = [
+              "system:serviceaccount:${var.commons_name}:external-secrets"
+            ]
+            "${var.oidc_provider_arn}:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+
+  path = "/gen3-service/"
+}
+
+
+resource "aws_iam_role_policy" "external-secrets-role-policy" {
+  name = "external-secrets-role-policy"
+  role = aws_iam_role.external-secrets-role[0].id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "secretsmanager:ListSecrets",
+          "secretsmanager:GetSecretValue"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "external-secrets-role-policy-attachment" {
+  role = aws_iam_role.external-secrets-role.name
+  policy_arn = aws_iam_role_policy.external-secrets-role-policy.arn
+}
---
+++ tf_files/aws/external-secrets/variables.tf
@@ -0,0 +1,5 @@
+variable commons_name {
+  description = "This is the name of the specific commons you are running. Each commons will only have access to its own secrets"
+}
+
+variable oidc_provider_arn {}
