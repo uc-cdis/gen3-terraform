177b7f61f8211618374b5737eb34b3dde7b0ba73
EliseCastle23 <109446148+EliseCastle23@users.noreply.github.com>
2025-08-21 09:40:12 -0600

    switching logic to match logic for the "default_csoc" resource.

--- tf_files/aws/modules/vpc/data.tf
+++ tf_files/aws/modules/vpc/data.tf
@@ -5,6 +5,6 @@ data "aws_availability_zones" "available" {}
 data "aws_caller_identity" "current" {}
 
 data "aws_route_tables" "control_routing_table" {
-  count   = var.csoc_managed ? 0 : 1
+  count   = var.csoc_managed ? 1 : 0
   vpc_id  = var.peering_vpc_id
 }
