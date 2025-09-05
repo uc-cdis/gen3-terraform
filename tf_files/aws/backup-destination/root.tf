8269c0d2dc519aa85e9300c827bcc252313f7696
Aidan Hilt <ahilt@uchicago.edu>
2025-09-05 17:03:52 -0400

    Add new module to prepare account as backup dest

---
+++ tf_files/aws/backup-destination/root.tf
@@ -0,0 +1,17 @@
+resource "aws_kms_key" "central_backup_key" {
+  description             = "KMS key for encrypting RDS backups from other accounts"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+}
+
+resource "aws_backup_vault" "account_vaults" {
+  for_each = toset(var.account_ids)
+  
+  name        = "rds-central-backup-vault-${each.key}"
+  kms_key_arn = aws_kms_key.backup_vault_key.arn
+  
+  tags = {
+    Name      = "rds-central-backup-vault-${each.key}"
+    AccountId = each.key
+  }
+}
