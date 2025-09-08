be159bf18a754ec141ab0b74b3aad1fe9c734242
Aidan Hilt <ahilt@uchicago.edu>
2025-09-08 11:25:39 -0400

    Fix current identity reference

--- tf_files/aws/modules/backup/cloud.tf
+++ tf_files/aws/modules/backup/cloud.tf
@@ -33,7 +33,7 @@ resource "aws_kms_key_policy" "backup_key_external_account" {
         Principal = {
           AWS = [
             "arn:aws:iam::${var.backup_destination_account}:root",
-            "arn:aws:iam::${data.aws_caller_identity.current}:root"
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
           ]
         }
 
