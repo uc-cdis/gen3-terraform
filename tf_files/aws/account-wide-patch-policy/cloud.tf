e985464ea031ffa68cceb27ebd57075c43ee50c7
Aidan Hilt <ahilt@uchicago.edu>
2025-09-10 11:49:39 -0400

    Hopefully this has default roles it can use

--- tf_files/aws/account-wide-patch-policy/cloud.tf
+++ tf_files/aws/account-wide-patch-policy/cloud.tf
@@ -24,9 +24,7 @@ resource "aws_ssmquicksetup_configuration_manager" "patch_policy_setup" {
   name = "primary-patch-policy"
 
   configuration_definition {
-    local_deployment_administration_role_arn = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/AWS-QuickSetup-PatchPolicy-LocalAdministrationRole"
-    local_deployment_execution_role_name     = "AWS-QuickSetup-PatchPolicy-LocalExecutionRole"
-    type                                     = "AWSQuickSetupType-PatchPolicy"
+    type = "AWSQuickSetupType-PatchPolicy"
 
     parameters = {
       "ConfigurationOptionsPatchOperation" : "Scan",
