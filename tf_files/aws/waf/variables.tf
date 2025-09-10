df6a8d0c9b50f607bc728e9fa7a4bc8f82d953ec
EliseCastle23 <109446148+EliseCastle23@users.noreply.github.com>
2025-09-10 10:43:33 -0600

    Updating base ruleset

--- tf_files/aws/waf/variables.tf
+++ tf_files/aws/waf/variables.tf
@@ -10,22 +10,55 @@ variable "base_rules" {
     managed_rule_group_name = string
     priority = number
     override_to_count = list(string)
+    count = bool
   }))
   default = [
     {
       managed_rule_group_name = "AWSManagedRulesAmazonIpReputationList"
       priority = 0
       override_to_count = ["AWSManagedReconnaissanceList"]
+      count = false
     },
     {
       managed_rule_group_name = "AWSManagedRulesPHPRuleSet"
       priority = 1
       override_to_count = ["PHPHighRiskMethodsVariables_HEADER", "PHPHighRiskMethodsVariables_QUERYSTRING", "PHPHighRiskMethodsVariables_BODY"]
+      count = false
     },
     {
       managed_rule_group_name = "AWSManagedRulesWordPressRuleSet"
       priority = 2
       override_to_count= ["WordPressExploitableCommands_QUERYSTRING", "WordPressExploitablePaths_URIPATH"]
+      count = false
+    },
+    {
+      managed_rule_group_name = "AWSManagedRulesAdminProtectionRuleSet"
+      priority = 3
+      override_to_count= ["AdminProtection_URIPATH"]
+    },
+    {
+      managed_rule_group_name = "AWSManagedRulesCommonRuleSet"
+      priority = 4
+      override_to_count= []
+      count = true
+    },
+    {
+      managed_rule_group_name = "AWSManagedRulesKnownBadInputsRuleSet"
+      priority = 5
+      override_to_count= []
+      count = true
+    },
+    {
+      managed_rule_group_name = "AWSManagedRulesLinuxRuleSet"
+      priority = 6
+      override_to_count= []
+      count = true
+    },
+    {
+      managed_rule_group_name = "AWSManagedRulesBotControlRuleSet"
+      priority = 7
+      override_to_count= []
+      count = true
     },
   ]
 }
