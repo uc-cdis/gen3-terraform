81f4ca6ae6d7f010c627b582cc33d597e39f030b
Aidan Hilt <ahilt@uchicago.edu>
2025-09-10 11:18:40 -0400

    Change targetRegion data type

--- tf_files/aws/account-wide-patch-policy/cloud.tf
+++ tf_files/aws/account-wide-patch-policy/cloud.tf
@@ -41,24 +41,7 @@ resource "aws_ssmquicksetup_configuration_manager" "patch_policy_setup" {
       "RateControlErrorThreshold" : "2%",
       "IsPolicyAttachAllowed" : "false",
       "TargetAccounts" : data.aws_caller_identity.current.account_id,
-      "TargetRegions" : [
-        "us-east-1",
-        "us-east-2",
-        "us-west-1",
-        "us-west-2",
-        "sa-east-1",
-        "eu-central-1",
-        "eu-west-1",
-        "eu-west-2",
-        "eu-west-3",
-        "eu-north-1",
-        "ca-central-1",
-        "ap-south-1",
-        "ap-northeast-1",
-        "ap-northeast-2",
-        "ap-southeast-1",
-        "ap-southeast-2"
-      ],
+      "TargetRegions" : "us-east-1,us-east-2,us-west-1,us-west-2,sa-east-1,eu-central-1,eu-west-1,eu-west-2,eu-west-3,eu-north-1,ca-central-1,ap-south-1,ap-northeast-1,ap-northeast-2,ap-southeast-1,ap-southeast-2",
       "TargetType" : "*"
     }
   }
