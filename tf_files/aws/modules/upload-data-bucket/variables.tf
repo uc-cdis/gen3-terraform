740453adc63223197689360712aa54d4e45cef6f
Ed <emalinowski@uchicago.edu>
2025-11-03 09:18:27 -0600

    fix(sns-sqs-kms): Updated the sns and sqs to use the same kms key

--- tf_files/aws/modules/upload-data-bucket/variables.tf
+++ tf_files/aws/modules/upload-data-bucket/variables.tf
@@ -13,13 +13,13 @@ variable "force_delete_bucket" {
   default     = false
 }
 
-variable "sqs_encryption_enabled" {
+variable "encryption_enabled" {
   description = "Enable server-side encryption for the SQS queue"
   type        = bool
   default     = true
 }
 
-variable "sqs_kms_key_id" {
+variable "kms_key_id" {
   description = "KMS Key ID to use for server-side encryption. If not provided, the default AWS managed key will be used."
   type        = string
   default     = ""
