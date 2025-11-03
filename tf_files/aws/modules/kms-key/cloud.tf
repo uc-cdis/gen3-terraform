8e8b2a9c07203f31fb03f9ce3314be76f64b4b9d
Ed <emalinowski@uchicago.edu>
2025-11-03 09:46:54 -0600

    fix(sns-sqs-kms): Updated the sns and sqs to use the same kms key

--- tf_files/aws/modules/kms-key/cloud.tf
+++ tf_files/aws/modules/kms-key/cloud.tf
@@ -1,31 +1,33 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  account_ids = distinct(compact(concat(
+    var.account_ids,
+    ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+  )))
+}
+
 resource "aws_kms_key" "kms_key" {}
+
+resource "aws_kms_alias" "kms_key_alias" {
+  name          = "alias/${var.alias_name}"
+  target_key_id = aws_kms_key.kms_key.id
+}
 
 resource "aws_kms_key_policy" "example" {
   key_id = aws_kms_key.kms_key.id
+
   policy = jsonencode({
+    Version = "2012-10-17"
     Statement = [
       {
-        Action = "kms:*"
-        Effect = "Allow"
-        Principal = {
-          AWS = "*"
-        }
-
-        Resource = "*"
-        Sid      = "Enable IAM User Permissions"
-      },
-      {
-        Action = var.action
-        Effect = "Allow"
-        Principal = {
-          AWS = var.account_ids
-        }
-
-        Resource = "*"
-        Sid      = "Allow use of the key"
-
-      },
+        Sid       = "Allow use of the key"
+        Effect    = "Allow"
+        Principal = { AWS = local.account_ids }
+        Action    = var.action
+        Resource  = "*"
+      }
     ]
-    Version = "2012-10-17"
   })
 }
+
