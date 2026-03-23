4b4d5058b016d20934b2a610b27fe669e221b750
Ubuntu <ubuntu@ip-10-0-0-14.us-east-2.compute.internal>
2026-03-23 21:15:56 +0000

    revert changes

--- tf_files/aws/modules/data-bucket-queue/data.tf
+++ tf_files/aws/modules/data-bucket-queue/data.tf
@@ -9,9 +9,6 @@ data "aws_iam_policy_document" "sns-topic-policy" {
   policy_id = "__default_policy_ID"
 
   statement {
-    sid    = "__default_statement_ID"
-    effect = "Allow"
-
     actions = [
       "SNS:Subscribe",
       "SNS:Receive",
@@ -20,38 +17,24 @@ data "aws_iam_policy_document" "sns-topic-policy" {
       "SNS:GetTopicAttributes",
     ]
 
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values    = [
+        "arn:aws:s3:*:*:${var.bucket_name}",
+      ]
+    }
+    effect = "Allow"
+
     principals {
       type        = "AWS"
       identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
     }
 
-    resources = [aws_sns_topic.user_updates.arn]
-  }
+    resources = [
+      aws_sns_topic.user_updates.arn,
+    ]
 
-  # Allow S3 to publish notifications to this topic
-  statement {
-    sid    = "s3_bucket_notification"
-    effect = "Allow"
-
-    actions = ["SNS:Publish"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
-
-    condition {
-      test     = "ArnLike"
-      variable = "aws:SourceArn"
-      values   = ["arn:aws:s3:::${var.bucket_name}"]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceAccount"
-      values   = [data.aws_caller_identity.current.account_id]
-    }
-
-    resources = [aws_sns_topic.user_updates.arn]
+    sid = "__default_statement_ID"
   }
 }
