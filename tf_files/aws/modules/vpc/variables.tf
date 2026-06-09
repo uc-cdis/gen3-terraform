c9da9900fcb3f986be5431565652fcff6f7ed9cb
Edward Malinowski <emalinowski@uchicago.edu>
2026-06-09 12:32:05 -0500

    Added support for custom squid startup scripts and overriding the aurora instance db name

--- tf_files/aws/modules/vpc/variables.tf
+++ tf_files/aws/modules/vpc/variables.tf
@@ -89,52 +89,53 @@ variable "branch" {
   default     = "master"
 }
 
-variable "ssh_keys_repo" {
+
+variable "squid_ssh_keys_repo" {
   description = "Optional repo containing squid authorized key files"
   default     = ""
 }
 
-variable "ssh_admin_keys_file" {
+variable "squid_ssh_admin_keys_file" {
   description = "Repo-relative path to admin authorized_keys file"
   default     = ""
 }
 
-variable "ssh_user_keys_file" {
+variable "squid_ssh_user_keys_file" {
   description = "Repo-relative path to sftp user authorized_keys file"
   default     = ""
 }
 
-variable "whitelist_repo" {
+variable "squid_whitelist_repo" {
   description = "Optional repo containing squid whitelist files"
   default     = ""
 }
 
-variable "ftp_whitelist_file" {
+variable "squid_ftp_whitelist_file" {
   description = "Repo-relative path to ftp whitelist file"
   default     = ""
 }
 
-variable "web_whitelist_file" {
+variable "squid_web_whitelist_file" {
   description = "Repo-relative path to web whitelist file"
   default     = ""
 }
 
-variable "web_wildcard_whitelist_file" {
+variable "squid_web_wildcard_whitelist_file" {
   description = "Repo-relative path to web wildcard whitelist file"
   default     = ""
 }
 
-variable "script_repo" {
+variable "squid_script_repo" {
   description = "Optional repo containing squid helper scripts"
   default     = ""
 }
 
-variable "updatewhitelist_script_file" {
+variable "squid_updatewhitelist_script_file" {
   description = "Repo-relative path to updatewhitelist script"
   default     = ""
 }
 
-variable "healthcheck_script_file" {
+variable "squid_healthcheck_script_file" {
   description = "Repo-relative path to healthcheck script"
   default     = ""
 }
