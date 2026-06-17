a8b24ddc4634562e7d9d6aee8fb28eaa8b718dd6
Edward Malinowski <emalinowski@uchicago.edu>
2026-06-17 09:47:16 -0500

    Updated sns topic policy

--- tf_files/aws/modules/data-bucket-queue/data.tf
+++ tf_files/aws/modules/data-bucket-queue/data.tf
@@ -9,32 +9,58 @@ data "aws_iam_policy_document" "sns-topic-policy" {
   policy_id = "__default_policy_ID"
 
   statement {
+    sid    = "DefaultStatementID"
+    effect = "Allow"
+
     actions = [
-      "SNS:Subscribe",
-      "SNS:Receive",
       "SNS:Publish",
+      "SNS:RemovePermission",
+      "SNS:SetTopicAttributes",
+      "SNS:DeleteTopic",
       "SNS:ListSubscriptionsByTopic",
       "SNS:GetTopicAttributes",
+      "SNS:Receive",
+      "SNS:AddPermission",
+      "SNS:Subscribe",
     ]
-
-    condition {
-      test     = "ArnLike"
-      variable = "aws:SourceArn"
-      values    = [
-        "arn:aws:s3:*:*:${var.bucket_name}",
-      ]
-    }
-    effect = "Allow"
 
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+      identifiers = ["*"]
     }
 
-    resources = [
-      aws_sns_topic.user_updates.arn,
-    ]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
 
-    sid = "__default_statement_ID"
+    resources = [aws_sns_topic.user_updates.arn]
+  }
+
+  statement {
+    sid    = "AllowS3Publish"
+    effect = "Allow"
+
+    actions = ["SNS:Publish"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    resources = [aws_sns_topic.user_updates.arn]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:s3:::${var.bucket_name}"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
   }
 }
