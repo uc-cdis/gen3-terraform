13b18cf0a957b753668dd27bf2e9d61e8b41ca35
Edward Malinowski <emalinowski@uchicago.edu>
2026-07-31 08:46:27 -0500

    Restricted policy a bit more

--- tf_files/gen3/service-accounts.tf
+++ tf_files/gen3/service-accounts.tf
@@ -848,40 +848,18 @@ resource "aws_iam_role_policy" "efs-csi-node-s3-policy" {
   count = var.namespace == "default" && var.deploy_s3_files ? 1 : 0
   name = "efs-csi-node-gen3wf-s3-policy"
   role = aws_iam_role.efs-csi-node-role[0].id
+
   policy = jsonencode({
     Version = "2012-10-17"
 
     Statement = [
       {
-        Sid    = "ListGen3WorkflowBuckets"
-        Effect = "Allow"
-
-        Action = [
-          "s3:GetBucketLocation",
-          "s3:GetBucketVersioning",
-          "s3:ListBucket",
-          "s3:ListBucketMultipartUploads",
-          "s3:ListBucketVersions"
-        ]
-
-        Resource = [
-          "arn:aws:s3:::gen3wf-*"
-        ]
-      },
-      {
-        Sid    = "ReadGen3WorkflowObjects"
+        Sid    = "S3ObjectReadAccess"
         Effect = "Allow"
 
         Action = [
           "s3:GetObject",
-          "s3:GetObjectAcl",
-          "s3:GetObjectAttributes",
-          "s3:GetObjectTagging",
-          "s3:GetObjectVersion",
-          "s3:GetObjectVersionAcl",
-          "s3:GetObjectVersionAttributes",
-          "s3:GetObjectVersionTagging",
-          "s3:ListMultipartUploadParts"
+          "s3:GetObjectVersion"
         ]
 
         Resource = [
@@ -889,22 +867,15 @@ resource "aws_iam_role_policy" "efs-csi-node-s3-policy" {
         ]
       },
       {
-        Sid    = "WriteGen3WorkflowObjects"
+        Sid    = "S3BucketListAccess"
         Effect = "Allow"
 
         Action = [
-          "s3:AbortMultipartUpload",
-          "s3:DeleteObject",
-          "s3:DeleteObjectTagging",
-          "s3:DeleteObjectVersion",
-          "s3:DeleteObjectVersionTagging",
-          "s3:PutObject",
-          "s3:PutObjectAcl",
-          "s3:PutObjectTagging"
+          "s3:ListBucket"
         ]
 
         Resource = [
-          "arn:aws:s3:::gen3wf-*/*"
+          "arn:aws:s3:::gen3wf-*"
         ]
       }
     ]
