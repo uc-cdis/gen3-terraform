25cb5a495ed0e6b0d4f72d2c1d084ff2ff950cd6
Alan Walsh <alwalsh@iu.edu>
2026-08-26 11:08:16 -0400

    Use unique names for SQS queues

--- tf_files/gen3/sqs.tf
+++ tf_files/gen3/sqs.tf
@@ -1,9 +1,9 @@
 module "audit-sqs" {
   source = "../aws/modules/sqs"
-  sqs_name = "audit"
+  sqs_name = "${var.vpc_name}-audit"
 }
 
 module "ssjdispatcher-sqs" {
   source = "../aws/modules/sqs"
-  sqs_name = "ssjdispatcher"
+  sqs_name = "${var.vpc_name}-ssjdispatcher"
 }
